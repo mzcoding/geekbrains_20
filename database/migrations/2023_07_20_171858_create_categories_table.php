1167a5dd1601bb57174d11b394a6e5ee7cd81896
Stanislav <stanislav.boyko@orderyoyo.com>
2023-07-20 21:31:09 +0300

    lesson 5 - migrations, seeders, db queries

--- database/migrations/2023_07_20_171858_create_categories_table.php
+++ database/migrations/2023_07_20_171858_create_categories_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->text('description')->nullable();
+            $table->timestamps();
         });
     }
 
@@ -25,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('categories');
     }
 };
--- database/migrations/2023_07_20_173800_add_description_column_in_news_table.php
+++ database/migrations/2023_07_20_173800_add_description_column_in_news_table.php
@@ -13,10 +13,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::table('news', function (Blueprint $table) {
+            $table->text('description')->after('status')->nullable();
         });
     }
 
@@ -25,6 +23,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::table('news', function (Blueprint $table) {
+            $table->dropColumn('description');
+        });
     }
 };
